d3774b82e482e6d3cd489764c15eef912891059a
geossophia <geovana.horodeski06@gmail.com>
2022-06-28 23:05:57 -0300

    gsh

--- admin/vue-project/src/views/LivrosView.vue
+++ admin/vue-project/src/views/LivrosView.vue
@@ -5,17 +5,33 @@ export default {
     return {
       livros: [{}],
       novo_livro: "",
+      novo_pre: "",
+      novo_quant: "",
     };
   },
   methods: {
     salvar() {
-      if (this.novo_livro !== "") {
+      if (
+        (this.novo_livro !== "", this.novo_pre !== "", this.novo_quant !== "")
+      ) {
         const novo_id = uuidv4();
+        const novo_ISBN = uuidv4();
+        const novo_categoria_ID = uuidv4();
+        const novo_editora_ID = uuidv4();
+        const novo_autor_ID = uuidv4();
         this.livros.push({
           id: novo_id,
           nome: this.novo_livro,
+          quantidade: this.novo_quant,
+          preco: this.novo_pre,
+          ISBN: novo_ISBN,
+          Categoria_ID: novo_categoria_ID,
+          Editora_ID: novo_editora_ID,
+          Autor_ID: novo_autor_ID,
         });
         this.novo_livro = "";
+        this.novo_quant = "";
+        this.novo_pre = "";
       }
     },
     excluir(livro) {
@@ -32,34 +48,51 @@ export default {
         <h2>Gerenciamento de livros</h2>
       </div>
       <div class="form-input">
-        <input type="text" v-model="novo_livro" />
+        <input
+          id="input_tit"
+          type="text"
+          v-model="novo_livro"
+          placeholder="Titulo"
+        />
+        <input
+          id="input_quant"
+          type="number"
+          v-model="novo_quant"
+          placeholder="Quantidade"
+        />
+        <input
+          id="input_pre"
+          type="number"
+          v-model="novo_pre"
+          placeholder="Preço"
+        />
         <button @click="salvar">Salvar</button>
       </div>
       <div class="list-times">
         <table>
           <thead>
             <tr>
-              <th>ID</th>
-              <th>Titulo</th>
-              <th>ISBN</th>
-              <th>Categoria_ID</th>
-              <th>Editora_ID</th>
-              <th>Autor_ID</th>
-              <th>Quantidade</th>
-              <th>Preço</th>
-              <th>Ação</th>
+              <td>ID</td>
+              <td>Titulo</td>
+              <td>ISBN</td>
+              <td>Categoria_ID</td>
+              <td>Editora_ID</td>
+              <td>Autor_ID</td>
+              <td>Quantidade</td>
+              <td>Preço</td>
+              <td>Ação</td>
             </tr>
           </thead>
           <tbody>
-            <tr v-for="livro in livros" :key="livro.id">
+            <tr v-for="livro in livros" :key="livro">
               <td>{{ livro.id }}</td>
               <td>{{ livro.nome }}</td>
               <td>{{ livro.ISBN }}</td>
               <td>{{ livro.Categoria_ID }}</td>
               <td>{{ livro.Editora_ID }}</td>
               <td>{{ livro.Autor_ID }}</td>
-              <td>{{ livro.Preco }}</td>
-              <td>{{ livro.acao }}</td>
+              <td>{{ livro.quantidade }}</td>
+              <td>{{ livro.preco }}</td>
               <td>
                 <button>editar</button>
                 <button @click="excluir(livro)">excluir</button>
