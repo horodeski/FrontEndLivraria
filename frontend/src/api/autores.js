fc73b4a6093cdbaaaac8e490850f2b9610fbc29c
paulozemr <pauloramosj213@gmail.com>
2022-08-31 14:39:54 -0300

    gsh

--- frontend/src/api/autores.js
+++ frontend/src/api/autores.js
@@ -1,28 +1,28 @@
 import axios from "axios";
 export default class AutoresApi {
   async buscarTodosOsAutores() {
-    const response = await axios.get("http://localhost:4000/autores");
+    const response = await axios.get("https://livrariadodudu2.herokuapp.com/autores");
     return response.data;
   }
 
   async buscarAutores(id) {
-    const response = await axios.get(`http://localhost:4000/autores/${id}`);
+    const response = await axios.get(`https://livrariadodudu2.herokuapp.com/autores/${id}`);
     return response.data;
   }
 
   async adicionarAutor(autor) {
-    const response = await axios.post("http://localhost:4000/autores", autor);
+    const response = await axios.post("https://livrariadodudu2.herokuapp.com/autores", autor);
     return response.data;
   }
 
   async excluirAutor(id) {
-    const response = await axios.delete(`http://localhost:4000/autores/${id}`);
+    const response = await axios.delete(`https://livrariadodudu2.herokuapp.com/autores/${id}`);
     return response.data;
   }
 
   async atualizarAutor(autor) {
     const response = await axios.put(
-      `http://localhost:4000/autores/${autor.id}`,
+      `https://livrariadodudu2.herokuapp.com/autores/${autor.id}`,
       autor
     );
     return response.data;
--- frontend/src/api/editoras.js
+++ frontend/src/api/editoras.js
@@ -1,31 +1,31 @@
 import axios from "axios";
 export default class AutoresApi {
   async buscarTodasAsEditoras() {
-    const response = await axios.get("http://localhost:4000/editoras");
+    const response = await axios.get("https://livrariadodudu2.herokuapp.com/editoras");
     return response.data;
   }
 
   async buscarEditora(id) {
-    const response = await axios.get(`http://localhost:4000/editoras/${id}`);
+    const response = await axios.get(`https://livrariadodudu2.herokuapp.com/editoras/${id}`);
     return response.data;
   }
 
   async adicionarEditora(editora) {
     const response = await axios.post(
-      "http://localhost:4000/editoras",
+      "https://livrariadodudu2.herokuapp.com/editoras",
       editora
     );
     return response.data;
   }
 
   async excluirEditora(id) {
-    const response = await axios.delete(`http://localhost:4000/editoras/${id}`);
+    const response = await axios.delete(`https://livrariadodudu2.herokuapp.com/editoras/${id}`);
     return response.data;
   }
 
   async atualizarEditora(editora) {
     const response = await axios.put(
-      `http://localhost:4000/editoras/${editora.id}`,
+      `https://livrariadodudu2.herokuapp.com/editoras/${editora.id}`,
       editora
     );
     return response.data;
